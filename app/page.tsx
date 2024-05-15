c80ebda29b805a34637991b6954f5add908ff425
Jason Savelli <jason.savelli@mention.com>
2024-05-16 00:23:36 +0200

    feat: prettify landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,57 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import { BOARD_COLUMN_SIZE } from './game/board/board-constants'
 import { SnakeCell } from './game/snake/snake-cell'
+import { AuroraBackground } from '@/components/ui/aurora-background'
+import { motion } from 'framer-motion'
+import { useCallback } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+    const router = useRouter()
+
+    const handleStartGame = useCallback(() => {
+        router.push('/game')
+    }, [router])
+
     return (
-        <main className="flex min-h-[100dvh] flex-col items-center gap-6 p-8">
-            <section
-                className="relative"
-                style={{ height: 5 * BOARD_COLUMN_SIZE, width: 3 * BOARD_COLUMN_SIZE }}
+        <AuroraBackground className="justify-start px-6 py-8">
+            <motion.div
+                initial={{ opacity: 0.0, y: 40 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{
+                    delay: 0.3,
+                    duration: 0.8,
+                    ease: 'easeInOut',
+                }}
+                className="relative flex w-full flex-col items-center gap-4 "
             >
-                <SnakeCell coordinate={{ x: 0, y: 0 }} />
-                <SnakeCell coordinate={{ x: 1, y: 0 }} />
-                <SnakeCell coordinate={{ x: 2, y: 0 }} />
-                <SnakeCell coordinate={{ x: 0, y: 1 }} />
-                <SnakeCell coordinate={{ x: 0, y: 2 }} />
-                <SnakeCell coordinate={{ x: 1, y: 2 }} />
-                <SnakeCell coordinate={{ x: 2, y: 2 }} />
-                <SnakeCell coordinate={{ x: 2, y: 3 }} />
-                <SnakeCell coordinate={{ x: 2, y: 4 }} />
-                <SnakeCell coordinate={{ x: 1, y: 4 }} />
-                <SnakeCell isHeadCell coordinate={{ x: 0, y: 4 }} />
-            </section>
+                <section
+                    className="relative"
+                    style={{ height: 5 * BOARD_COLUMN_SIZE, width: 3 * BOARD_COLUMN_SIZE }}
+                >
+                    <SnakeCell coordinate={{ x: 0, y: 0 }} />
+                    <SnakeCell coordinate={{ x: 1, y: 0 }} />
+                    <SnakeCell coordinate={{ x: 2, y: 0 }} />
+                    <SnakeCell coordinate={{ x: 0, y: 1 }} />
+                    <SnakeCell coordinate={{ x: 0, y: 2 }} />
+                    <SnakeCell coordinate={{ x: 1, y: 2 }} />
+                    <SnakeCell coordinate={{ x: 2, y: 2 }} />
+                    <SnakeCell coordinate={{ x: 2, y: 3 }} />
+                    <SnakeCell coordinate={{ x: 2, y: 4 }} />
+                    <SnakeCell coordinate={{ x: 1, y: 4 }} />
+                    <SnakeCell isHeadCell coordinate={{ x: 0, y: 4 }} />
+                </section>
 
-            <h1 className="text-accent text-5xl font-bold uppercase">Sneak</h1>
+                <h1 className="text-5xl font-bold uppercase text-accent">Sneak</h1>
 
-            <Button>Play</Button>
-        </main>
+                <section className="w-full max-w-[400px]">
+                    <Button className="w-full" onClick={handleStartGame}>
+                        Play
+                    </Button>
+                </section>
+            </motion.div>
+        </AuroraBackground>
     )
 }
